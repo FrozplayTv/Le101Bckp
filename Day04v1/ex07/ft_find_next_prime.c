678f82bb58879e16e75f2f688a9a2b6f5ec02918
Lilian Mangin <limangin@z1r9p4.le-101.fr>
2017-09-20 21:30:06 +0200

    Piscine C

---
+++ Day04v1/ex07/ft_find_next_prime.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: limangin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/19 16:05:47 by limangin          #+#    #+#             */
+/*   Updated: 2017/09/19 16:44:56 by limangin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_prime(int nb)
+{
+	int i;
+
+	i = 2;
+	if ((nb <= 1) || (nb > 2147483647))
+		return (0);
+	while (i <= nb / i)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	if (ft_is_prime(nb))
+		return (nb);
+	while (ft_is_prime(nb) == 0)
+	{
+		nb++;
+	}
+	return (nb);
+}
